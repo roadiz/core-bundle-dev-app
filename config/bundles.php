8842ba0a17db16a53ff657e6c124c7ef1cfbe5ad
Ambroise Maupate <ambroise@rezo-zero.com>
2021-09-08 18:22:03 +0200

    Added JMS serializer, and custom dirs

--- config/bundles.php
+++ config/bundles.php
@@ -12,5 +12,6 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    \RZ\Roadiz\CoreBundle\RoadizCoreBundle::class => ['all' => true],
+    RZ\Roadiz\CoreBundle\RoadizCoreBundle::class => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
 ];
--- src/Kernel.php
+++ src/Kernel.php
@@ -2,13 +2,12 @@
 
 namespace App;
 
-use RZ\Roadiz\Core\Models\FileAwareInterface;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
-class Kernel extends BaseKernel implements FileAwareInterface
+class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 
@@ -36,46 +35,4 @@ class Kernel extends BaseKernel implements FileAwareInterface
             $routes->import('../config/{routes}.php');
         }
     }
-
-    public function getPublicFilesPath(): string
-    {
-        return $this->getProjectDir() . '/public' . $this->getPublicFilesBasePath();
-    }
-
-    public function getPublicFilesBasePath(): string
-    {
-        return '/files';
-    }
-
-    public function getPrivateFilesPath(): string
-    {
-        $this->getProjectDir() . '/var' . $this->getPrivateFilesBasePath();
-    }
-
-    public function getPrivateFilesBasePath(): string
-    {
-        return '/files/private';
-    }
-
-    public function getFontsFilesPath(): string
-    {
-        $this->getProjectDir() . '/var' . $this->getFontsFilesBasePath();
-    }
-
-    public function getFontsFilesBasePath(): string
-    {
-        return '/files/fonts';
-    }
-
-    public function getPublicCachePath(): string
-    {
-        $this->getProjectDir() . '/public' . $this->getPublicCacheBasePath();
-    }
-
-    public function getPublicCacheBasePath(): string
-    {
-        return '/assets';
-    }
-
-
 }
