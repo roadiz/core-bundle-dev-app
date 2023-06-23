fe0960fe3a076a9618e4ea3ae67624f37bb33cb9
Ambroise Maupate <ambroise@rezo-zero.com>
2023-06-24 00:41:41 +0200

    feat: Moved Models namespace to src
    BREAKING CHANGE: roadiz/models namespace root is now ./src. Change your Doctrine entities path

--- lib/RoadizRozierBundle/src/Aliases.php
+++ lib/RoadizRozierBundle/src/Aliases.php
@@ -12,7 +12,6 @@ final class Aliases
     public static function getAliases(): array
     {
         return [
-            \RZ\Roadiz\RozierBundle\Controller\BackendController::class => \RZ\Roadiz\CMS\Controllers\BackendController::class,
             \RZ\Roadiz\RozierBundle\Form\DocumentTranslationType::class => \Themes\Rozier\Forms\DocumentTranslationType::class,
             \RZ\Roadiz\RozierBundle\Form\CustomFormType::class => \Themes\Rozier\Forms\CustomFormType::class,
             \RZ\Roadiz\RozierBundle\ListManager\SessionListFilters::class => \Themes\Rozier\Utils\SessionListFilters::class,
