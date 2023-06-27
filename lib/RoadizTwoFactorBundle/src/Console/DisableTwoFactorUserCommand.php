81cd4720dea1c545741147ecca7e5e80723796db
Ambroise Maupate <ambroise@rezo-zero.com>
2023-06-27 23:23:23 +0200

    feat(TwoFactorBundle): Added console command to disable 2FA and override users:list command

---
+++ lib/RoadizTwoFactorBundle/src/Console/DisableTwoFactorUserCommand.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RZ\Roadiz\TwoFactorBundle\Console;
+
+use Doctrine\Persistence\ManagerRegistry;
+use RZ\Roadiz\CoreBundle\Console\UsersCommand;
+use RZ\Roadiz\TwoFactorBundle\Entity\TwoFactorUser;
+use RZ\Roadiz\TwoFactorBundle\Security\Provider\TwoFactorUserProviderInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'users:2fa:disable',
+    description: 'Disable two-factor authentication for a user.',
+)]
+final class DisableTwoFactorUserCommand extends UsersCommand
+{
+    private TwoFactorUserProviderInterface $twoFactorUserProvider;
+
+    public function __construct(
+        ManagerRegistry $managerRegistry,
+        TwoFactorUserProviderInterface $twoFactorUserProvider,
+        string $name = null
+    ) {
+        parent::__construct($managerRegistry, $name);
+        $this->twoFactorUserProvider = $twoFactorUserProvider;
+        $this->managerRegistry = $managerRegistry;
+    }
+
+    protected function configure(): void
+    {
+        $this->addArgument(
+            'username',
+            InputArgument::REQUIRED,
+            'Username'
+        );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $name = $input->getArgument('username');
+        $user = $this->getUserForInput($input);
+
+        $twoFactorUser = $this->twoFactorUserProvider->getFromUser($user);
+
+        if (!$twoFactorUser instanceof TwoFactorUser) {
+            $io->warning('User “' . $name . '” does not have two-factor authentication enabled.');
+            return 1;
+        }
+
+        $this->twoFactorUserProvider->disable($twoFactorUser);
+        $io->success('Two-factor authentication disabled for user “' . $name . '”.');
+
+        return 0;
+    }
+
+}
