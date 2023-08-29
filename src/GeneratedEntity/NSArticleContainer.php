2a0fa7fca1191e306b0e55f5d32b41117cbd45ef
Ambroise Maupate <ambroise@rezo-zero.com>
2023-08-29 18:49:36 +0200

    chore: Added ArticleContainer node-type

---
+++ src/GeneratedEntity/NSArticleContainer.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * THIS IS A GENERATED FILE, DO NOT EDIT IT
+ * IT WILL BE RECREATED AT EACH NODE-TYPE UPDATE
+ */
+namespace App\GeneratedEntity;
+
+use Doctrine\Common\Collections\Collection;
+use JMS\Serializer\Annotation as Serializer;
+use Symfony\Component\Serializer\Annotation as SymfonySerializer;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Doctrine\Orm\Filter as OrmFilter;
+use ApiPlatform\Serializer\Filter\PropertyFilter;
+
+/**
+ * DO NOT EDIT
+ * Generated custom node-source type by Roadiz.
+ */
+#[
+    Gedmo\Loggable(logEntryClass: \RZ\Roadiz\CoreBundle\Entity\UserLogEntry::class),
+    ORM\Entity(repositoryClass: \App\GeneratedEntity\Repository\NSArticleContainerRepository::class),
+    ORM\Table(name: "ns_articlecontainer"),
+    ApiFilter(PropertyFilter::class)
+]
+class NSArticleContainer extends \RZ\Roadiz\CoreBundle\Entity\NodesSources
+{
+    #[
+        Serializer\VirtualProperty,
+        Serializer\Groups(["nodes_sources", "nodes_sources_default"]),
+        Serializer\SerializedName("@type"),
+        SymfonySerializer\Groups(["nodes_sources", "nodes_sources_default"]),
+        SymfonySerializer\SerializedName(serializedName: "@type")
+    ]
+    public function getNodeTypeName(): string
+    {
+        return 'ArticleContainer';
+    }
+
+    /**
+     * $this->nodeType->isReachable() proxy.
+     *
+     * @return bool Does this nodeSource is reachable over network?
+     */
+    public function isReachable(): bool
+    {
+        return true;
+    }
+
+    /**
+     * $this->nodeType->isPublishable() proxy.
+     *
+     * @return bool Does this nodeSource is publishable with date and time?
+     */
+    public function isPublishable(): bool
+    {
+        return false;
+    }
+
+    public function __toString(): string
+    {
+        return '[NSArticleContainer] ' . parent::__toString();
+    }
+}
