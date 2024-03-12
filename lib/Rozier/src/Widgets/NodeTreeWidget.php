61625f1b24c61f133a18953e8d78c04021ff0340
Ambroise Maupate <ambroise@rezo-zero.com>
2024-03-12 23:08:52 +0100

    chore: Improve NodesSources.php `getOneDisplayableDocument`

--- lib/Rozier/src/Widgets/NodeTreeWidget.php
+++ lib/Rozier/src/Widgets/NodeTreeWidget.php
@@ -184,13 +184,12 @@ final class NodeTreeWidget extends AbstractWidget
             /*
              * Stored in session
              */
-            $sessionListFilter = new SessionListFilters(static::SESSION_ITEM_PER_PAGE);
+            $sessionListFilter = new SessionListFilters(self::SESSION_ITEM_PER_PAGE);
             $sessionListFilter->handleItemPerPage($this->getRequest(), $listManager);
         } else {
             $listManager->setItemPerPage(99999);
             $listManager->handle(true);
         }
-
 
         if ($subRequest) {
             $listManager->disablePagination();
