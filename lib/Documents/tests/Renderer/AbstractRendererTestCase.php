f8092dce03b81a3d523d4a44b56a6e5796b08d0e
Ambroise Maupate <ambroise@rezo-zero.com>
2024-07-11 16:50:11 +0200

    fix: Fixed unit tests namespaces, moved to tests folder and use `autoload-dev` composer directive

---
+++ lib/Documents/tests/Renderer/AbstractRendererTestCase.php
@@ -0,0 +1,94 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RZ\Roadiz\Documents\Tests\Renderer;
+
+use League\Flysystem\Config;
+use League\Flysystem\Filesystem;
+use League\Flysystem\FilesystemOperator;
+use League\Flysystem\Local\LocalFilesystemAdapter;
+use League\Flysystem\MountManager;
+use League\Flysystem\UrlGeneration\PublicUrlGenerator;
+use PHPUnit\Framework\TestCase;
+use RZ\Roadiz\Documents\MediaFinders\EmbedFinderFactory;
+use RZ\Roadiz\Documents\Tests\MediaFinders\SimpleVimeoEmbedFinder;
+use RZ\Roadiz\Documents\Tests\MediaFinders\SimpleYoutubeEmbedFinder;
+use RZ\Roadiz\Documents\UrlGenerators\DocumentUrlGeneratorInterface;
+use RZ\Roadiz\Documents\UrlGenerators\DummyDocumentUrlGenerator;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+abstract class AbstractRendererTestCase extends TestCase
+{
+    protected function htmlTidy(string $body): string
+    {
+        $body = preg_replace('#[\n\r\t\s]{2,}#', ' ', $body);
+        $body = str_replace("&#x2F;", '/', $body);
+        $body = html_entity_decode($body);
+        return preg_replace('#\>[\n\r\t\s]+\<#', '><', $body);
+    }
+
+    protected function getUrlGenerator(): DocumentUrlGeneratorInterface
+    {
+        return new DummyDocumentUrlGenerator();
+    }
+
+    protected function getFilesystemOperator(): FilesystemOperator
+    {
+        return new MountManager([
+            'public' => new Filesystem(
+                new LocalFilesystemAdapter(dirname(__DIR__) . '/../files/'),
+                publicUrlGenerator: new class () implements PublicUrlGenerator
+                {
+                    public function publicUrl(string $path, Config $config): string
+                    {
+                        return '/files/' . $path;
+                    }
+                }
+            ),
+            'private' => new Filesystem(
+                new LocalFilesystemAdapter(dirname(__DIR__) . '/../files/'),
+                publicUrlGenerator: new class () implements PublicUrlGenerator
+                {
+                    public function publicUrl(string $path, Config $config): string
+                    {
+                        return '/files/' . $path;
+                    }
+                }
+            )
+        ]);
+    }
+
+    protected function getEnvironment(): Environment
+    {
+        $loader = new FilesystemLoader([
+            dirname(__DIR__) . '/../src/Resources/views'
+        ]);
+        return new Environment($loader, [
+            'autoescape' => false
+        ]);
+    }
+
+
+
+    /**
+     * @return EmbedFinderFactory
+     */
+    protected function getEmbedFinderFactory(): EmbedFinderFactory
+    {
+        return new EmbedFinderFactory([
+            'youtube' => SimpleYoutubeEmbedFinder::class,
+            'vimeo' => SimpleVimeoEmbedFinder::class,
+        ]);
+    }
+
+    public function assertHtmlTidyEquals(string $expected, string $actual, string $message = ''): void
+    {
+        $this->assertEquals(
+            $this->htmlTidy($expected),
+            $this->htmlTidy($actual),
+            $message
+        );
+    }
+}
---
+++ lib/Models/tests/StringHandlerTest.php
@@ -0,0 +1,290 @@
+<?php
+
+namespace RZ\Roadiz\Tests;
+
+use PHPUnit\Framework\TestCase;
+use RZ\Roadiz\Utils\StringHandler;
+
+class StringHandlerTest extends TestCase
+{
+    /**
+     * @dataProvider cleanForFilenameProvider
+     */
+    public function testCleanForFilename(string $input, string $expected): void
+    {
+        $this->assertEquals($expected, StringHandler::cleanForFilename($input));
+    }
+
+    public function cleanForFilenameProvider(): array
+    {
+        return [
+            [
+                "Les-Echos_26022015_Les-entrepreneurs-partent-à-lassaut-du-secteur-bancaire.pdf",
+                "les_echos_26022015_les_entrepreneurs_partent_a_lassaut_du_secteur_bancaire.pdf"
+            ],
+            [
+                "Les-entrepreneurs-partent-à-lassaut-du-secteur-bancaire.pdf",
+                "les_entrepreneurs_partent_a_lassaut_du_secteur_bancaire.pdf"
+            ],
+            [
+                "image.jpg",
+                "image.jpg",
+            ],
+            [
+                "image with spaces.jpg",
+                "image_with_spaces.jpg",
+            ],
+            [
+                "image/with/slashes.jpg",
+                "image_with_slashes.jpg",
+            ]
+        ];
+    }
+
+    /**
+     * @dataProvider endsWithProvider
+     */
+    public function testEndsWith(string $input, string $wanted, bool $expected): void
+    {
+        $this->assertEquals($expected, StringHandler::endsWith($input, $wanted));
+    }
+
+    public function endsWithProvider(): array
+    {
+        return [
+            ["  ", "Locale", false],
+            ["", "Locale", false],
+            ["home", "Locale", false],
+            ["ocale", "Locale", false],
+            ["testPage", "Locale", false],
+            ["localePage", "Locale", false],
+            ["testLocalePage", "Locale", false],
+            ["testPageLocale", "Locale", true],
+            ["testPagelocale", "Locale", false],
+            ["testPageGateau", "Locale", false],
+            ["testPage", "", true],
+            ["LocaletestPage", "Locale", false],
+        ];
+    }
+
+    /**
+     * @dataProvider replaceLastProvider
+     */
+    public function testReplaceLast(string $input, string $wanted, string $expected): void
+    {
+        $this->assertEquals($expected, StringHandler::replaceLast($wanted, "", $input));
+    }
+
+    /**
+     * @return array
+     */
+    public function replaceLastProvider(): array
+    {
+        return [
+            ["testPage", "Locale", "testPage"],
+            ["localePage", "Locale", "localePage"],
+            ["testLocalePage", "Locale", "testPage"],
+            ["testPageLocale", "Locale", "testPage"],
+            ["testPagelocale", "Locale", "testPagelocale"],
+            ["testPageGateau", "Locale", "testPageGateau"],
+            ["testPage", "", "testPage"],
+            ["LocalePage", "Locale", "Page"],
+        ];
+    }
+
+    /**
+     * @dataProvider removeDiacriticsProvider
+     */
+    public function testRemoveDiacritics(string $input, string $expected): void
+    {
+        // Assert
+        $this->assertEquals($expected, StringHandler::removeDiacritics($input));
+    }
+
+    /**
+     * @return array
+     */
+    public function removeDiacriticsProvider(): array
+    {
+        return [
+            ["à", "a"],
+            ["é", "e"],
+            ["À", "A"],
+            ["É", "E"],
+            ["œ", "oe"],
+            ["ç", "c"],
+            ["__à", "__a"],
+            ["--é", "--e"],
+            [
+                "Les-echos_26022015_Les-entrepreneurs-partent-à-lassaut-du-secteur-bancaire.pdf",
+                "Les-echos_26022015_Les-entrepreneurs-partent-a-lassaut-du-secteur-bancaire.pdf"
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider variablizeProvider
+     */
+    public function testVariablize(string $input, string $expected): void
+    {
+        // Assert
+        $this->assertEquals($expected, StringHandler::variablize($input));
+    }
+
+    /**
+     * @return array
+     */
+    public function variablizeProvider(): array
+    {
+        return [
+            ["à", "a"],
+            ["é", "e"],
+            ["À", "a"],
+            ["É", "e"],
+            ["œ", "oe"],
+            ["ç", "c"],
+            ["__à", "a"],
+            ["--é", "e"],
+            ["Ligula  $* _--Egestas Mattis Nullam$* _  ", "ligula_egestas_mattis_nullam"],
+            ["Véèsti buœlum Rïsus+", "veesti_buoelum_risus"],
+            ["J'aime les sushis!", "j_aime_les_sushis"],
+            ["J’aime les sushis!", "j_aime_les_sushis"],
+            ["J'aime les\n sushis!\t\n", "j_aime_les_sushis"],
+            ["?header_image", "header_image"],
+            ["JAime les_sushis", "j_aime_les_sushis"],
+            ["Ébène", "ebene"],
+            ["ébène", "ebene"],
+        ];
+    }
+
+    /**
+     * @dataProvider classifyProvider
+     */
+    public function testClassify(string $input, string $expected): void
+    {
+        // Assert
+        $this->assertEquals($expected, StringHandler::classify($input));
+    }
+
+    /**
+     * @return array
+     */
+    public function classifyProvider(): array
+    {
+        return [
+            ["Ligula  $* _--Egestas Mattis Nullam", "LigulaEgestasMattisNullam"],
+            ["Véèsti buœlum Rïsus", "VeestiBuoelumRisus"],
+            ["J'aime les sushis", "JAimeLesSushis"],
+            ["header_image", "HeaderImage"],
+            ["JAime les_sushis", "JAimeLesSushis"],
+        ];
+    }
+
+    /**
+     * @dataProvider camelCaseProvider
+     */
+    public function testCamelCase(string $input, string $expected): void
+    {
+        // Assert
+        $this->assertEquals($expected, StringHandler::camelcase($input));
+    }
+
+    /**
+     * @return array
+     */
+    public function camelCaseProvider(): array
+    {
+        return [
+            ["Ligula  $* _--Egestas Mattis Nullam", "ligulaEgestasMattisNullam"],
+            ["Véèsti buœlum Rïsus", "veestiBuoelumRisus"],
+            ["J'aime les sushis", "jAimeLesSushis"],
+            ["header_image", "headerImage"],
+            ["JAime les_sushis", "jAimeLesSushis"],
+        ];
+    }
+
+    /**
+     * @dataProvider slugifyProvider
+     */
+    public function testSlugify(string $input, string $expected): void
+    {
+        // Assert
+        $this->assertEquals($expected, StringHandler::slugify($input));
+    }
+
+    /**
+     * @return array
+     */
+    public function slugifyProvider(): array
+    {
+        return [
+            ["Ligula  $* _--Egestas Mattis Nullam$* _  ", "ligula-egestas-mattis-nullam"],
+            ["Véèsti buœlum Rïsus+", "veesti-buoelum-risus"],
+            ["veesti-buoelum-risus", "veesti-buoelum-risus"],
+            ["J'aime les sushis!", "j-aime-les-sushis"],
+            ["J’aime les sushis!", "j-aime-les-sushis"],
+            ["J'aime les\n sushis!\t\n", "j-aime-les-sushis"],
+            ["?header_image", "header-image"],
+            ["JAime les_sushis", "jaime-les-sushis"],
+            ["Ébène", "ebene"],
+            ["ébène", "ebene"],
+            ["Page1 1", "page1-1"],
+            ["Page3", "page3"],
+            ["Page 3", "page-3"],
+            ["Page 3 3", "page-3-3"],
+            ["12 Page 3 3", "12-page-3-3"],
+            ["straßburg", "strassburg"]
+        ];
+    }
+
+    /**
+     * @dataProvider encodeWithSecretProvider
+     */
+    public function testEncodeWithSecret(string $input, string $secret): void
+    {
+        $code = StringHandler::encodeWithSecret($input, $secret);
+
+        // Assert
+        $this->assertEquals($input, StringHandler::decodeWithSecret($code, $secret));
+    }
+
+    /**
+     * @return array
+     */
+    public function encodeWithSecretProvider(): array
+    {
+        return [
+            ["Ligula  $* _--Egestas Mattis Nullam", "Commodo Pellentesque Sem Fusce Quam"],
+            ["Véèsti buœlum Rïsus ", "  change#this#secret#very#important"],
+            ["J'aime les sushis  ", " Fringilla Vulputate Dolor Inceptos"],
+            ["au   " . PHP_EOL . "ietaui.\\eauie@auietsrt.trr", "Sit Vestibulum Dolor Ullamcorper Aenean"],
+            ["JAime les_sushis", "Sit Vestibulum Dolor"],
+        ];
+    }
+
+    /**
+     * @dataProvider encodeWithSecretNoSaltProvider
+     */
+    public function testEncodeWithSecretNoSalt(string $input, string $secret): void
+    {
+        $this->expectException('\\InvalidArgumentException');
+
+        $code = StringHandler::encodeWithSecret($input, $secret);
+
+        // Assert
+        $this->assertEquals($input, StringHandler::decodeWithSecret($code, $secret));
+    }
+
+    /**
+     * @return array
+     */
+    public function encodeWithSecretNoSaltProvider(): array
+    {
+        return [
+            ["Ligula  $* _--Egestas Mattis Nullam", ""],
+            ["Véèsti buœlum Rïsus ", "  "],
+            ["J'aime les sushis  ", "  "],
+            ["auietauieauie@auietsrt.trr", PHP_EOL],
+        ];
+    }
+}
