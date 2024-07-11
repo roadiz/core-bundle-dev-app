06548e2f129a22c96a81b248f725a98e62fe54af
Ambroise Maupate <ambroise@rezo-zero.com>
2024-07-12 01:50:55 +0200

    fix: Get rid of `cssAction` route and use CSS vars for node, tag, attribute, custom-form, folder accent colors

--- lib/Rozier/src/RozierServiceRegistry.php
+++ lib/Rozier/src/RozierServiceRegistry.php
@@ -17,57 +17,30 @@ use Themes\Rozier\Widgets\TreeWidgetFactory;
 
 final class RozierServiceRegistry
 {
-    private Settings $settingsBag;
-    private ManagerRegistry $managerRegistry;
-    private TreeWidgetFactory $treeWidgetFactory;
-    private NodeChrootResolver $chrootResolver;
-    private array $backofficeMenuEntries;
-
     private ?array $settingGroups = null;
     private ?TagTreeWidget $tagTree = null;
     private ?FolderTreeWidget $folderTree = null;
     private ?NodeTreeWidget $nodeTree = null;
 
-    /**
-     * @param Settings $settingsBag
-     * @param ManagerRegistry $managerRegistry
-     * @param TreeWidgetFactory $treeWidgetFactory
-     * @param NodeChrootResolver $chrootResolver
-     * @param array $backofficeMenuEntries
-     */
     public function __construct(
-        Settings $settingsBag,
-        ManagerRegistry $managerRegistry,
-        TreeWidgetFactory $treeWidgetFactory,
-        NodeChrootResolver $chrootResolver,
-        array $backofficeMenuEntries
+        private readonly Settings $settingsBag,
+        private readonly ManagerRegistry $managerRegistry,
+        private readonly TreeWidgetFactory $treeWidgetFactory,
+        private readonly NodeChrootResolver $chrootResolver,
+        private readonly array $backofficeMenuEntries
     ) {
-        $this->settingsBag = $settingsBag;
-        $this->managerRegistry = $managerRegistry;
-        $this->treeWidgetFactory = $treeWidgetFactory;
-        $this->chrootResolver = $chrootResolver;
-        $this->backofficeMenuEntries = $backofficeMenuEntries;
     }
 
-    /**
-     * @return int|float
-     */
-    public function getMaxFilesize()
+    public function getMaxFilesize(): int|float
     {
         return UploadedFile::getMaxFilesize();
     }
 
-    /**
-     * @return DocumentInterface|null
-     */
     public function getAdminImage(): ?DocumentInterface
     {
         return $this->settingsBag->getDocument('admin_image');
     }
 
-    /**
-     * @return array
-     */
     public function getSettingGroups(): array
     {
         if (null === $this->settingGroups) {
@@ -96,11 +69,7 @@ final class RozierServiceRegistry
         return $this->folderTree;
     }
 
-    /**
-     * @param mixed $user
-     * @return NodeTreeWidget
-     */
-    public function getNodeTree($user): NodeTreeWidget
+    public function getNodeTree(mixed $user): NodeTreeWidget
     {
         if (null === $this->nodeTree) {
             $this->nodeTree = $this->treeWidgetFactory->createNodeTree(
@@ -110,9 +79,6 @@ final class RozierServiceRegistry
         return $this->nodeTree;
     }
 
-    /**
-     * @return array
-     */
     public function getBackofficeMenuEntries(): array
     {
         return $this->backofficeMenuEntries;
