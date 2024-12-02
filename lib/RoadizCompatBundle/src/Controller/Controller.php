a79d46b1f843820e9e7484e1981591b6d50a2310
Ambroise Maupate <ambroise@rezo-zero.com>
2024-12-02 22:51:24 +0100

    refactor: Delete unused CompatBundle Controller methods
    BREAKING CHANGE: Delete Controller::findTranslationForLocale, Controller::renderJson, Controller::denyResourceExceptForFormats, Controller::getHandlerFactory, Controller::getPreviewResolver methods

--- lib/RoadizCompatBundle/src/Controller/Controller.php
+++ lib/RoadizCompatBundle/src/Controller/Controller.php
@@ -5,28 +5,25 @@ declare(strict_types=1);
 namespace RZ\Roadiz\CompatBundle\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\NonUniqueResultException;
 use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\Persistence\ObjectManager;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 use Psr\Log\LoggerInterface;
 use RZ\Roadiz\Core\AbstractEntities\PersistableInterface;
-use RZ\Roadiz\Core\AbstractEntities\TranslationInterface;
 use RZ\Roadiz\Core\Handlers\HandlerFactoryInterface;
 use RZ\Roadiz\CoreBundle\Bag\NodeTypes;
 use RZ\Roadiz\CoreBundle\Bag\Roles;
 use RZ\Roadiz\CoreBundle\Bag\Settings;
 use RZ\Roadiz\CoreBundle\Entity\NodesSources;
-use RZ\Roadiz\CoreBundle\Entity\Translation;
 use RZ\Roadiz\CoreBundle\EntityApi\NodeApi;
 use RZ\Roadiz\CoreBundle\EntityApi\NodeSourceApi;
 use RZ\Roadiz\CoreBundle\Exception\ForceResponseException;
-use RZ\Roadiz\CoreBundle\Exception\NoTranslationAvailableException;
 use RZ\Roadiz\CoreBundle\Form\Error\FormErrorSerializer;
 use RZ\Roadiz\CoreBundle\ListManager\EntityListManager;
 use RZ\Roadiz\CoreBundle\ListManager\EntityListManagerInterface;
 use RZ\Roadiz\CoreBundle\Node\NodeFactory;
 use RZ\Roadiz\CoreBundle\Preview\PreviewResolverInterface;
-use RZ\Roadiz\CoreBundle\Repository\TranslationRepository;
 use RZ\Roadiz\CoreBundle\SearchEngine\Indexer\NodeIndexer;
 use RZ\Roadiz\CoreBundle\SearchEngine\NodeSourceSearchHandlerInterface;
 use RZ\Roadiz\CoreBundle\Security\Authorization\Chroot\NodeChrootResolver;
@@ -40,7 +37,6 @@ use Symfony\Cmf\Component\Routing\RouteObjectInterface;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormFactoryInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
@@ -180,19 +176,8 @@ abstract class Controller extends AbstractController
     }
 
     /**
-     * @deprecated
-     */
-    protected function getPreviewResolver(): PreviewResolverInterface
-    {
-        /** @var PreviewResolverInterface $previewResolver */
-        $previewResolver = $this->container->get(PreviewResolverInterface::class);
-
-        return $previewResolver;
-    }
-
-    /**
-     * @throws \Psr\Container\ContainerExceptionInterface
-     * @throws \Psr\Container\NotFoundExceptionInterface
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
      *
      * @deprecated
      */
@@ -228,17 +213,6 @@ abstract class Controller extends AbstractController
     /**
      * @deprecated
      */
-    protected function getHandlerFactory(): HandlerFactoryInterface
-    {
-        /** @var HandlerFactoryInterface $handlerFactory */ // php-stan hint
-        $handlerFactory = $this->container->get(HandlerFactoryInterface::class);
-
-        return $handlerFactory;
-    }
-
-    /**
-     * @deprecated
-     */
     protected function getLogger(): LoggerInterface
     {
         /** @var LoggerInterface $logger */ // php-stan hint
@@ -251,6 +225,9 @@ abstract class Controller extends AbstractController
      * Wrap `$this->get('urlGenerator')->generate`.
      *
      * @param string|NodesSources $route
+     *
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
      */
     protected function generateUrl($route, array $parameters = [], int $referenceType = UrlGeneratorInterface::ABSOLUTE_PATH): string
     {
@@ -296,35 +273,6 @@ abstract class Controller extends AbstractController
     }
 
     /**
-     * @throws NonUniqueResultException
-     */
-    protected function findTranslationForLocale(?string $_locale = null): TranslationInterface
-    {
-        if (null === $_locale) {
-            $defaultTranslation = $this->getDoctrine()->getRepository(Translation::class)->findDefault();
-            if (null === $defaultTranslation) {
-                throw new NoTranslationAvailableException();
-            }
-
-            return $defaultTranslation;
-        }
-        /** @var TranslationRepository $repository */
-        $repository = $this->getDoctrine()->getRepository(Translation::class);
-
-        if ($this->getPreviewResolver()->isPreview()) {
-            $translation = $repository->findOneByLocaleOrOverrideLocale($_locale);
-        } else {
-            $translation = $repository->findOneAvailableByLocaleOrOverrideLocale($_locale);
-        }
-
-        if (null !== $translation) {
-            return $translation;
-        }
-
-        throw new NoTranslationAvailableException();
-    }
-
-    /**
      * Return a Response from a template string with its rendering assignation.
      *
      * @see http://api.symfony.com/2.6/Symfony/Bundle/FrameworkBundle/Controller/Controller.html#method_render
@@ -358,23 +306,6 @@ abstract class Controller extends AbstractController
         return $view;
     }
 
-    public function renderJson(array $data = [], int $httpStatus = Response::HTTP_OK): JsonResponse
-    {
-        return $this->json($data, $httpStatus);
-    }
-
-    /**
-     * Throw a NotFoundException if request format is not accepted.
-     *
-     * @return void
-     */
-    protected function denyResourceExceptForFormats(Request $request, array $acceptableFormats = ['html'])
-    {
-        if (!in_array($request->get('_format', 'html'), $acceptableFormats)) {
-            throw $this->createNotFoundException(sprintf('Resource not found for %s format', $request->get('_format', 'html')));
-        }
-    }
-
     /**
      * Creates and returns a form builder instance.
      *
@@ -382,11 +313,9 @@ abstract class Controller extends AbstractController
      * @param mixed  $data    The initial data for the form
      * @param array  $options Options for the form
      *
-     * @return FormBuilderInterface
-     *
      * @deprecated Use constructor service injection
      */
-    protected function createNamedFormBuilder(string $name = 'form', $data = null, array $options = [])
+    protected function createNamedFormBuilder(string $name = 'form', mixed $data = null, array $options = []): FormBuilderInterface
     {
         /** @var FormFactoryInterface $formFactory */
         $formFactory = $this->container->get(FormFactoryInterface::class);
