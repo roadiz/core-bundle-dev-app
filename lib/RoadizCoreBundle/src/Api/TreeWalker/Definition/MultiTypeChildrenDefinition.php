cd8b4c145bc748105ece7b50ddc554c26a03e756
Ambroise Maupate <ambroise@rezo-zero.com>
2025-02-10 11:24:19 +0100

    fix: Migrate all Node-type related TreeWalker definition from NodeSourceApi to QueryBuilder and NodeTypes bag

--- lib/RoadizCoreBundle/src/Api/TreeWalker/Definition/MultiTypeChildrenDefinition.php
+++ lib/RoadizCoreBundle/src/Api/TreeWalker/Definition/MultiTypeChildrenDefinition.php
@@ -4,57 +4,51 @@ declare(strict_types=1);
 
 namespace RZ\Roadiz\CoreBundle\Api\TreeWalker\Definition;
 
-use Doctrine\ORM\Tools\Pagination\Paginator;
 use RZ\Roadiz\CoreBundle\Api\TreeWalker\NodeSourceWalkerContext;
+use RZ\Roadiz\CoreBundle\Bag\NodeTypes;
 use RZ\Roadiz\CoreBundle\Entity\NodesSources;
+use RZ\Roadiz\CoreBundle\Entity\NodeType;
 use RZ\TreeWalker\Definition\ContextualDefinitionTrait;
 use RZ\TreeWalker\WalkerContextInterface;
 
 final class MultiTypeChildrenDefinition
 {
     use ContextualDefinitionTrait;
-
-    private array $types;
-    private bool $onlyVisible;
+    use NodeSourceDefinitionTrait;
 
     /**
-     * @param WalkerContextInterface $context
      * @param array<string> $types
-     * @param bool $onlyVisible
      */
-    public function __construct(WalkerContextInterface $context, array $types, bool $onlyVisible = true)
-    {
-        $this->context = $context;
-        $this->types = $types;
-        $this->onlyVisible = $onlyVisible;
+    public function __construct(
+        private WalkerContextInterface $context,
+        private array $types,
+        private bool $onlyVisible = true,
+    ) {
     }
 
     /**
-     * @param NodesSources $source
-     * @return array|Paginator
+     * @return array<NodeType> $nodeTypes
      */
-    public function __invoke(NodesSources $source)
+    protected function getNodeTypes(NodeTypes $nodeTypesBag): array
     {
-        if ($this->context instanceof NodeSourceWalkerContext) {
-            $this->context->getStopwatch()->start(self::class);
-            $bag = $this->context->getNodeTypesBag();
-            $criteria = [
-                'node.parent' => $source->getNode(),
-                'translation' => $source->getTranslation(),
-                'node.nodeType' => array_map(function (string $singleType) use ($bag) {
-                    return $bag->get($singleType);
-                }, $this->types)
-            ];
-            if ($this->onlyVisible) {
-                $criteria['node.visible'] = true;
-            }
-            $children = $this->context->getNodeSourceApi()->getBy($criteria, [
-                'node.position' => 'ASC',
-            ]);
-            $this->context->getStopwatch()->stop(self::class);
+        return array_values(array_filter(array_map(function (string $singleType) use ($nodeTypesBag) {
+            return $nodeTypesBag->get($singleType);
+        }, $this->types)));
+    }
 
-            return $children;
+    /**
+     * @return array<NodesSources>
+     */
+    public function __invoke(NodesSources $source): array
+    {
+        if (!($this->context instanceof NodeSourceWalkerContext)) {
+            throw new \InvalidArgumentException('Context should be instance of '.NodeSourceWalkerContext::class);
         }
-        throw new \InvalidArgumentException('Context should be instance of ' . NodeSourceWalkerContext::class);
+
+        $this->context->getStopwatch()->start(self::class);
+        $queryBuilder = $this->getQueryBuilder($source);
+        $this->context->getStopwatch()->stop(self::class);
+
+        return $queryBuilder->getQuery()->getResult();
     }
 }
