333ceed9870558ab73b008d63528db5e7efb3d20
eliot lauger <eliot@rezo-zero.com>
2025-04-01 11:22:41 +0200

    chore: phpcs

--- lib/Rozier/src/Forms/DocumentEditType.php
+++ lib/Rozier/src/Forms/DocumentEditType.php
@@ -20,7 +20,6 @@ use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormError;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\Validator\Constraints\LessThanOrEqual;
