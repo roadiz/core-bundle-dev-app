686225da98f5efab06cb9a433b11ea887a6e4190
Ambroise Maupate <ambroise@rezo-zero.com>
2025-06-10 19:21:29 +0200

    fix(Rozier): Update response status code for validation errors and refine template logic

--- importmap.php
+++ importmap.php
@@ -1,9 +1,19 @@
 <?php
-declare(strict_types=1);
 
+/**
+ * Returns the importmap for this application.
+ *
+ * - "path" is a path inside the asset mapper system. Use the
+ *     "debug:asset-map" command to see the full list of paths.
+ *
+ * - "entrypoint" (JavaScript only) set to true for any module that will
+ *     be used as an "entrypoint" (and passed to the importmap() Twig function).
+ *
+ * The "importmap:require" command can be used to add new entries to this file.
+ */
 return [
     'bundles/roadizrozier' => [
-        'path' => './lib/RoadizRozierBundle/public/app.js',
+        'path' => './lib/RoadizRozierBundle/static/app.js',
         'entrypoint' => true,
     ],
 ];
