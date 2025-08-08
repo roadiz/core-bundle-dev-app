9a08413575d16a9b900822fd80048afc53a75ff9
Ambroise Maupate <ambroise@rezo-zero.com>
2025-08-08 11:24:41 +0200

    fix(User): remove user credential expiration when changing password

--- lib/RoadizCoreBundle/src/Form/CreatePasswordType.php
+++ lib/RoadizCoreBundle/src/Form/CreatePasswordType.php
@@ -8,7 +8,6 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints as Assert;
 
 final class CreatePasswordType extends RepeatedType
 {
