113b5b4022ac5a60cc944434d1a9f4f90747b0b6
eliot lauger <eliot@rezo-zero.com>
2025-09-15 15:40:37 +0200

    feat: Use deepl sdk

--- lib/RoadizRozierBundle/src/Translator/DeeplTranslatorController.php
+++ lib/RoadizRozierBundle/src/Translator/DeeplTranslatorController.php
@@ -28,20 +28,18 @@ final readonly class DeeplTranslatorController implements TranslatorInterface
             throw new DeepLException('DeepL api key is required.');
         }
 
-        // $deeplClient = new DeepLClient($this->apiKey);
+        $deeplClient = new DeepLClient($this->apiKey);
 
-        // if (!in_array($translatorDto->targetLang, array_map(fn (Language $language) => $language->code, $deeplClient->getTargetLanguages()))) {
-        //     throw new DeepLException('Invalid target language.');
-        // }
+        if (!in_array($translatorDto->targetLang, array_map(fn (Language $language) => $language->code, $deeplClient->getTargetLanguages()))) {
+            throw new DeepLException('Invalid target language.');
+        }
 
-        // $result = $deeplClient->translateText($translatorDto->text, $translatorDto->sourceLang, $translatorDto->targetLang, $translatorDto->options);
+        $result = $deeplClient->translateText($translatorDto->text, $translatorDto->sourceLang, $translatorDto->targetLang, $translatorDto->options);
 
         return new TranslatorOutput(
             $translatorDto->text,
-            // $result->text,
-            'Bonjour monde',
-            // $result->detectedSourceLang,
-            'en-GB',
+            $result->text,
+            $result->detectedSourceLang,
             $translatorDto->targetLang,
         );
     }
