9c128b682c3bc84af0acf31a489bc262f0534282
timothe joubert <timothejoubert26@gmail.com>
2025-10-01 11:39:44 +0200

    chore: sort export declaration

--- lib/Rozier/vite-plugins/iconify/collections/ri.ts
+++ lib/Rozier/vite-plugins/iconify/collections/ri.ts
@@ -1,10 +1,10 @@
 export default [
-	'arrow-drop-up-line',
-	'arrow-drop-right-line',
-	'arrow-drop-down-line',
-	'arrow-drop-left-line',
 	'add-large-line',
 	'add-line',
+	'arrow-drop-down-line',
+	'arrow-drop-left-line',
+	'arrow-drop-right-line',
+	'arrow-drop-up-line',
 	'close-large-line',
 	'close-line',
 ]
