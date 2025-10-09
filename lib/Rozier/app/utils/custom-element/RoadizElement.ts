bcee5ef4b4b7829b845bbb41250b52f144da3486
Manuel Odelain <4610549+manuelodelain@users.noreply.github.com>
2025-10-09 08:54:31 +0200

    chore: lint files

--- lib/Rozier/app/utils/custom-element/RoadizElement.ts
+++ lib/Rozier/app/utils/custom-element/RoadizElement.ts
@@ -1,24 +1,24 @@
 interface Listener {
-  callback: EventListener
-  options: AddEventListenerOptions | boolean
-  boundCallback: EventListener
+    callback: EventListener
+    options: AddEventListenerOptions | boolean
+    boundCallback: EventListener
 }
 
 export default class RoadizElement extends HTMLElement {
-  _listeners: Map<Element, Map<string, Listener[]>>
+    _listeners: Map<Element, Map<string, Listener[]>>
 
-  constructor() {
-    super()
+    constructor() {
+        super()
 
-    this._listeners = new Map()
-  }
+        this._listeners = new Map()
+    }
 
-  disconnectedCallback() {
-    // Clean up all event listeners
-    this.unlistenAll()
-  }
+    disconnectedCallback() {
+        // Clean up all event listeners
+        this.unlistenAll()
+    }
 
-  /**
+    /**
      * Add event listener(s) to target element(s) with automatic cleanup
      * @param target - Element or collection of elements to listen to
      * @param eventType - Event type to listen for
@@ -26,121 +26,147 @@ export default class RoadizElement extends HTMLElement {
      * @param options - Event listener options
      * @param args - Additional arguments to bind to the callback
      */
-  listen(
-    target: Element | ArrayLike<Element>,
-    eventType: string,
-    callback: EventListener,
-    options: AddEventListenerOptions | boolean = false,
-    ...args: unknown[]
-  ) {
-    if (!target) {
-      console.warn(`Target ${target} is not valid for ${eventType} listener`)
-      return
+    listen(
+        target: Element | ArrayLike<Element>,
+        eventType: string,
+        callback: EventListener,
+        options: AddEventListenerOptions | boolean = false,
+        ...args: unknown[]
+    ) {
+        if (!target) {
+            console.warn(
+                `Target ${target} is not valid for ${eventType} listener`,
+            )
+            return
+        }
+
+        if (!eventType) {
+            console.warn(
+                `Event type ${eventType} is not valid for listener on target ${target}`,
+            )
+            return
+        }
+
+        if (typeof callback !== 'function') {
+            console.warn(
+                `Callback for ${eventType} listener on target ${target} is not a function`,
+            )
+            return
+        }
+
+        // Normalize target to an array
+        const targetList = 'length' in target ? Array.from(target) : [target]
+
+        for (let i = 0; i < targetList.length; i++) {
+            const targetItem = targetList[i]
+
+            if (!this._listeners.has(targetItem)) {
+                this._listeners.set(targetItem, new Map())
+            }
+
+            const targetListeners = this._listeners.get(targetItem)!
+
+            if (!targetListeners.has(eventType)) {
+                targetListeners.set(eventType, [])
+            }
+
+            const eventTypeListeners = targetListeners.get(eventType)!
+
+            if (
+                eventTypeListeners.some(
+                    (listener) => listener.callback === callback,
+                )
+            ) {
+                const callbackName =
+                    callback.name && callback.name.length > 0
+                        ? callback.name
+                        : 'anonymous function'
+
+                console.warn(
+                    `Listener for type ${eventType} with callback ${callbackName} already exists`,
+                )
+                return
+            }
+
+            const boundCallback = callback.bind(this, ...args)
+
+            eventTypeListeners.push({ callback, options, boundCallback })
+
+            targetItem.addEventListener(eventType, boundCallback, options)
+        }
     }
 
-    if (!eventType) {
-      console.warn(`Event type ${eventType} is not valid for listener on target ${target}`)
-      return
-    }
-
-    if (typeof callback !== 'function') {
-      console.warn(`Callback for ${eventType} listener on target ${target} is not a function`)
-      return
-    }
-
-    // Normalize target to an array
-    const targetList = 'length' in target ? Array.from(target) : [target]
-
-    for (let i = 0; i < targetList.length; i++) {
-      const targetItem = targetList[i]
-
-      if (!this._listeners.has(targetItem)) {
-        this._listeners.set(targetItem, new Map())
-      }
-
-      const targetListeners = this._listeners.get(targetItem)!
-
-      if (!targetListeners.has(eventType)) {
-        targetListeners.set(eventType, [])
-      }
-
-      const eventTypeListeners = targetListeners.get(eventType)!
-
-      if (eventTypeListeners.some(listener => listener.callback === callback)) {
-        const callbackName = callback.name && callback.name.length > 0 ? callback.name : 'anonymous function'
-
-        console.warn(`Listener for type ${eventType} with callback ${callbackName} already exists`)
-        return
-      }
-
-      const boundCallback = callback.bind(this, ...args)
-
-      eventTypeListeners.push({ callback, options, boundCallback })
-
-      targetItem.addEventListener(eventType, boundCallback, options)
-    }
-  }
-
-  /**
+    /**
      * Remove event listener(s) from target element(s)
      * @param target - Element or collection of elements to unlisten from
      * @param eventType - Optional event type to stop listening for. If not provided, all event types will be unlistened
      * @param callback - Optional event handler function to remove. If not provided, all callbacks for the event type will be removed
      */
-  unlisten(target: Element | ArrayLike<Element>, eventType?: string, callback?: EventListener) {
-    if (!target) return
+    unlisten(
+        target: Element | ArrayLike<Element>,
+        eventType?: string,
+        callback?: EventListener,
+    ) {
+        if (!target) return
 
-    // Normalize target to an array
-    const targetList = 'length' in target ? Array.from(target) : [target]
+        // Normalize target to an array
+        const targetList = 'length' in target ? Array.from(target) : [target]
 
-    for (let i = 0; i < targetList.length; i++) {
-      const targetItem = targetList[i]
+        for (let i = 0; i < targetList.length; i++) {
+            const targetItem = targetList[i]
 
-      if (!this._listeners.has(targetItem)) {
-        continue
-      }
-
-      const targetListeners = this._listeners.get(targetItem)!
-
-      targetListeners.forEach((listeners, listenerEventType) => {
-        if (!eventType || listenerEventType === eventType) {
-          listeners.forEach((listener) => {
-            if (!callback || listener.callback === callback) {
-              targetItem.removeEventListener(listenerEventType, listener.boundCallback, listener.options)
+            if (!this._listeners.has(targetItem)) {
+                continue
             }
-          })
 
-          if (callback) {
-            const filteredListeners = listeners.filter(listener => listener.callback !== callback)
+            const targetListeners = this._listeners.get(targetItem)!
 
-            if (filteredListeners.length > 0) {
-              targetListeners.set(listenerEventType, filteredListeners)
+            targetListeners.forEach((listeners, listenerEventType) => {
+                if (!eventType || listenerEventType === eventType) {
+                    listeners.forEach((listener) => {
+                        if (!callback || listener.callback === callback) {
+                            targetItem.removeEventListener(
+                                listenerEventType,
+                                listener.boundCallback,
+                                listener.options,
+                            )
+                        }
+                    })
+
+                    if (callback) {
+                        const filteredListeners = listeners.filter(
+                            (listener) => listener.callback !== callback,
+                        )
+
+                        if (filteredListeners.length > 0) {
+                            targetListeners.set(
+                                listenerEventType,
+                                filteredListeners,
+                            )
+                        } else {
+                            targetListeners.delete(listenerEventType) // Clean up empty event type entries
+                        }
+                    } else {
+                        targetListeners.delete(listenerEventType)
+                    }
+                }
+            })
+
+            // Clean up empty target entries
+            if (targetListeners.size === 0) {
+                this._listeners.delete(targetItem)
             }
-            else {
-              targetListeners.delete(listenerEventType) // Clean up empty event type entries
-            }
-          }
-          else {
-            targetListeners.delete(listenerEventType)
-          }
         }
-      })
-
-      // Clean up empty target entries
-      if (targetListeners.size === 0) {
-        this._listeners.delete(targetItem)
-      }
     }
-  }
 
-  /**
+    /**
      * Remove all event listeners from all targets
      */
-  unlistenAll() {
-    this._listeners.forEach((_listeners, target) => {
-      this.unlisten(target)
-    })
+    unlistenAll() {
+        this._listeners.forEach((_listeners, target) => {
+            this.unlisten(target)
+        })
 
-    this._listeners.clear()
-  }
+        this._listeners.clear()
+    }
 }
--- lib/Rozier/stories/icon/icon.stories.ts
+++ lib/Rozier/stories/icon/icon.stories.ts
@@ -4,54 +4,54 @@ import rzIconNames from '../../vite-plugins/iconify/collections/rz'
 import { iconItemRenderer, iconRenderer, type IconArgs } from './iconItem'
 
 const meta: Meta<IconArgs> = {
-  title: 'Integration/Icon',
-  argTypes: {
-    prefix: {
-      options: ['rz', 'ri'],
-      control: { type: 'radio' },
+    title: 'Integration/Icon',
+    argTypes: {
+        prefix: {
+            options: ['rz', 'ri'],
+            control: { type: 'radio' },
+        },
+        RzName: {
+            options: rzIconNames,
+            control: { type: 'select' },
+            if: { arg: 'prefix', eq: 'rz' },
+        },
+        RiName: {
+            options: riIconNames,
+            control: { type: 'select' },
+            if: { arg: 'prefix', eq: 'ri' },
+        },
+        fontSize: {
+            control: 'text',
+            description: 'A CSS unit font-size',
+        },
     },
-    RzName: {
-      options: rzIconNames,
-      control: { type: 'select' },
-      if: { arg: 'prefix', eq: 'rz' },
-    },
-    RiName: {
-      options: riIconNames,
-      control: { type: 'select' },
-      if: { arg: 'prefix', eq: 'ri' },
-    },
-    fontSize: {
-      control: 'text',
-      description: 'A CSS unit font-size',
-    },
-  },
-  tags: ['autodocs'],
+    tags: ['autodocs'],
 }
 
 export default meta
 type Story = StoryObj<IconArgs>
 
 export const Primary: Story = {
-  render: (args) => {
-    return iconRenderer(args)
-  },
-  parameters: {
-    layout: 'centered',
-  },
-  args: {
-    prefix: 'rz',
-    RzName: rzIconNames[0],
-    RiName: riIconNames[0],
-    color: '',
-    fontSize: '',
-  },
+    render: (args) => {
+        return iconRenderer(args)
+    },
+    parameters: {
+        layout: 'centered',
+    },
+    args: {
+        prefix: 'rz',
+        RzName: rzIconNames[0],
+        RiName: riIconNames[0],
+        color: '',
+        fontSize: '',
+    },
 }
 
 function getCollectionStory(prefix: string, names: string[]) {
-  return {
-    render: (args) => {
-      const container = document.createElement('div')
-      container.style = `
+    return {
+        render: (args) => {
+            const container = document.createElement('div')
+            container.style = `
         display: grid;
         grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
         gap: 42px;
@@ -59,21 +59,26 @@ function getCollectionStory(prefix: string, names: string[]) {
         max-width: 1000px;
         `
 
-      names.forEach((name) => {
-        const item = iconItemRenderer({ ...args, prefix, RzName: name, RiName: name })
-        container.appendChild(item)
-      })
+            names.forEach((name) => {
+                const item = iconItemRenderer({
+                    ...args,
+                    prefix,
+                    RzName: name,
+                    RiName: name,
+                })
+                container.appendChild(item)
+            })
 
-      return container
-    },
-    args: {
-      color: '',
-      fontSize: '',
-    },
-    parameters: {
-      controls: { exclude: ['prefix', 'RzName', 'RiName'] },
-    },
-  } as Story
+            return container
+        },
+        args: {
+            color: '',
+            fontSize: '',
+        },
+        parameters: {
+            controls: { exclude: ['prefix', 'RzName', 'RiName'] },
+        },
+    } as Story
 }
 
 export const RzCollection: Story = getCollectionStory('rz', rzIconNames)
