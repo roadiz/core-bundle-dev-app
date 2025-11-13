1f6009a45bad75d135ea389dadeeae8341b69bc3
timothe joubert <timothejoubert26@gmail.com>
2025-11-13 14:23:26 +0100

    feat: update style and stories

--- lib/Rozier/stories/RzDrawerBody.stories.ts
+++ lib/Rozier/stories/RzDrawerBody.stories.ts
@@ -1,13 +1,39 @@
 import type { Meta, StoryObj } from '@storybook/html-vite'
 import type { Args as DrawerItemArgs } from './RzDrawerItem.stories'
-import { rzDrawerItemRenderer } from '~/utils/storybook/renderer/rzDrawerItem'
-import image from './assets/images/01.jpg'
+import { rzDrawerItemRenderer } from '../app/utils/storybook/renderer/rzDrawerItem'
+import imageHorizontal from './assets/images/01.jpg'
+import imageVertical from './assets/images/02.jpg'
 
 const COMPONENT_CLASS_NAME = 'rz-drawer-body'
 
 type Args = {
     ariaLabel?: string
     items: DrawerItemArgs[]
+    moreColumns?: boolean
+}
+
+const DEFAULT_ITEM: DrawerItemArgs = {
+    overtitle: 'Overtitle example',
+    title: 'Title example',
+    image: {
+        src: imageVertical,
+        width: 110,
+        height: 94,
+    },
+    buttonGroup: {
+        gap: 'sm',
+        size: 'sm',
+        buttons: [
+            {
+                iconClass: 'rz-icon-ri--equalizer-3-line',
+                emphasis: 'primary',
+            },
+            {
+                iconClass: 'rz-icon-ri--delete-bin-7-line',
+                color: 'error-light',
+            },
+        ],
+    },
 }
 
 const SIMPLE_ITEM: DrawerItemArgs = {
@@ -31,7 +57,7 @@ const SIMPLE_ITEM: DrawerItemArgs = {
 
 const IMAGE_ITEM: DrawerItemArgs = {
     image: {
-        src: image,
+        src: imageHorizontal,
         width: 110,
         height: 94,
     },
@@ -65,7 +91,8 @@ const meta: Meta<Args> = {
     title: 'Components/Drawer/body',
     tags: ['autodocs'],
     args: {
-        items: [...Array(10).keys()].map(() => SIMPLE_ITEM),
+        items: [...Array(10).keys()].map(() => DEFAULT_ITEM),
+        moreColumns: false,
     },
 }
 
@@ -75,6 +102,8 @@ type Story = StoryObj<Args>
 function rzDrawerBodyRenderer(args: Args) {
     const wrapper = document.createElement('div')
     wrapper.classList.add(COMPONENT_CLASS_NAME)
+    if (args.moreColumns)
+        wrapper.classList.add(`${COMPONENT_CLASS_NAME}--more-columns`)
 
     args.items.forEach((itemArgs) => {
         const itemNode = rzDrawerItemRenderer(itemArgs)
@@ -88,10 +117,27 @@ export const Default: Story = {
     render: (args) => {
         return rzDrawerBodyRenderer(args)
     },
-    args: {},
 }
 
-export const OneItem: Story = {
+export const OneDefault: Story = {
+    render: (args) => {
+        return rzDrawerBodyRenderer(args)
+    },
+    args: {
+        items: [DEFAULT_ITEM],
+    },
+}
+
+export const SimpleItems: Story = {
+    render: (args) => {
+        return rzDrawerBodyRenderer(args)
+    },
+    args: {
+        items: [...Array(10).keys()].map(() => SIMPLE_ITEM),
+    },
+}
+
+export const SimpleItem: Story = {
     render: (args) => {
         return rzDrawerBodyRenderer(args)
     },
@@ -106,14 +152,84 @@ export const Images: Story = {
     },
     args: {
         items: [...Array(10).keys()].map(() => IMAGE_ITEM),
+        moreColumns: true,
     },
 }
 
-export const OneImage: Story = {
+export const Image: Story = {
     render: (args) => {
         return rzDrawerBodyRenderer(args)
     },
     args: {
-        items: [IMAGE_ITEM],
+        items: [
+            IMAGE_ITEM,
+            {
+                ...IMAGE_ITEM,
+                image: {
+                    src: imageVertical,
+                    width: 88,
+                    height: 110,
+                },
+            },
+        ],
+        moreColumns: true,
+    },
+}
+
+export const WithPictures: Story = {
+    render: (args) => {
+        return rzDrawerBodyRenderer(args)
+    },
+    args: {
+        moreColumns: true,
+        items: [
+            {
+                ...IMAGE_ITEM,
+                image: {
+                    src: imageHorizontal,
+                    width: 110,
+                    height: 94,
+                    sources: [
+                        {
+                            type: 'image/webp',
+                            srcset: imageVertical,
+                        },
+                    ],
+                },
+            },
+            {
+                ...IMAGE_ITEM,
+                image: {
+                    src: imageVertical,
+                    width: 110,
+                    height: 94,
+                    sources: [
+                        {
+                            type: 'image/webp',
+                            srcset: imageHorizontal,
+                        },
+                    ],
+                },
+            },
+        ],
+    },
+}
+
+export const Mixed: Story = {
+    render: (args) => {
+        return rzDrawerBodyRenderer(args)
+    },
+    args: {
+        items: [
+            DEFAULT_ITEM,
+            SIMPLE_ITEM,
+            SIMPLE_ITEM,
+            DEFAULT_ITEM,
+            DEFAULT_ITEM,
+            SIMPLE_ITEM,
+            SIMPLE_ITEM,
+            SIMPLE_ITEM,
+            SIMPLE_ITEM,
+        ],
     },
 }
