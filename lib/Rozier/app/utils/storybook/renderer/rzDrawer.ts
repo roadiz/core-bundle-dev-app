8939e77d553595777a12b7da8abae1d780d2e92f
timothe joubert <timothejoubert26@gmail.com>
2025-11-17 16:46:55 +0100

    refactor: update rzDrawerItemRenderer parameters

--- lib/Rozier/app/utils/storybook/renderer/rzDrawer.ts
+++ lib/Rozier/app/utils/storybook/renderer/rzDrawer.ts
@@ -9,55 +9,52 @@ const COMPONENT_CLASS_NAME = 'rz-drawer'
 
 export function rzDrawerItemRenderer(
     args: RzDrawerItemArgs,
-    COMPONENT_CLASS_NAME = 'rz-drawer__item',
+    itemClass = `${COMPONENT_CLASS_NAME}__item`,
 ) {
     const wrapper = document.createElement('div')
-    wrapper.classList.add(COMPONENT_CLASS_NAME)
+    wrapper.classList.add(itemClass)
 
     if (args.overtitle) {
         const overtitle = document.createElement('div')
-        overtitle.classList.add(`${COMPONENT_CLASS_NAME}__overtitle`)
+        overtitle.classList.add(`${itemClass}__overtitle`)
         overtitle.textContent = args.overtitle
         wrapper.appendChild(overtitle)
     }
 
     if (args.title) {
         const title = document.createElement('div')
-        title.classList.add(`${COMPONENT_CLASS_NAME}__title`)
+        title.classList.add(`${itemClass}__title`)
         title.textContent = args.title
         wrapper.appendChild(title)
     }
 
     if (args.image || args.badge) {
         const assetWrapper = document.createElement('div')
-        assetWrapper.classList.add(`${COMPONENT_CLASS_NAME}__asset`)
+        assetWrapper.classList.add(`${itemClass}__asset`)
         wrapper.appendChild(assetWrapper)
 
         if (args.image) {
             const imageNode = rzImageRenderer(args.image)
-            imageNode.classList.add(`${COMPONENT_CLASS_NAME}__img`)
+            imageNode.classList.add(`${itemClass}__img`)
             assetWrapper.appendChild(imageNode)
         }
 
         if (args.badge) {
             const badgeNode = rzBadgeRenderer(args.badge)
-            badgeNode.classList.add(`${COMPONENT_CLASS_NAME}__badge`)
+            badgeNode.classList.add(`${itemClass}__badge`)
             assetWrapper.appendChild(badgeNode)
         }
     }
 
     if (args.buttonGroup) {
         const node = rzButtonGroupRenderer(args.buttonGroup)
-        node.classList.add(`${COMPONENT_CLASS_NAME}__action`)
+        node.classList.add(`${itemClass}__action`)
         wrapper.appendChild(node)
     }
 
     if (args.buttonGroupTop) {
         const node = rzButtonGroupRenderer(args.buttonGroupTop)
-        node.classList.add(
-            `${COMPONENT_CLASS_NAME}__action`,
-            `${COMPONENT_CLASS_NAME}__action--top`,
-        )
+        node.classList.add(`${itemClass}__action`, `${itemClass}__action--top`)
         wrapper.appendChild(node)
     }
 
@@ -78,9 +75,9 @@ export function rzDrawerRenderer(args: RzDrawerArgs) {
     body.classList.add(`${COMPONENT_CLASS_NAME}__body`)
     wrapper.appendChild(body)
 
+    const itemClass = `${COMPONENT_CLASS_NAME}__item`
     args.items.forEach((itemArgs) => {
-        const itemNode = rzDrawerItemRenderer(itemArgs)
-        itemNode.classList.add(`${COMPONENT_CLASS_NAME}__item`)
+        const itemNode = rzDrawerItemRenderer(itemArgs, itemClass)
         body.appendChild(itemNode)
     })
 
