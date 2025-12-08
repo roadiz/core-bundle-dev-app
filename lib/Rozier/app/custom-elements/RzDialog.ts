93a8cd8c2585929114ad6e25222db4517eef7ed2
timothe joubert <timothejoubert26@gmail.com>
2025-12-08 15:23:20 +0100

    feat: add component and style

---
+++ lib/Rozier/app/custom-elements/RzDialog.ts
@@ -0,0 +1,65 @@
+export class RzDialog extends HTMLDialogElement {
+    constructor() {
+        super()
+
+        this.display = this.display.bind(this)
+        this.hide = this.hide.bind(this)
+    }
+
+    static get observedAttributes() {
+        return []
+    }
+
+    display() {
+        const isNonModal = this.hasAttribute('rz-dialog-non-modal')
+        if (isNonModal) {
+            this.show()
+        } else {
+            this.showModal()
+        }
+    }
+
+    hide() {
+        this.close()
+    }
+
+    attributeChangedCallback() {}
+
+    getTargets(state: 'open' | 'close') {
+        const id = this.getAttribute('id')
+        const innerTargets = this.querySelectorAll(
+            `[rz-dialog-${state}-target]`,
+        )
+        const outerTargets = document.querySelectorAll(
+            `[rz-dialog-${state}-target="${id}"]`,
+        )
+
+        return [
+            ...Array.from(innerTargets),
+            ...Array.from(outerTargets),
+        ].filter((el) => el)
+    }
+
+    connectedCallback() {
+        const openTargets = this.getTargets('open')
+        openTargets?.forEach((target) => {
+            target.addEventListener('click', this.display)
+        })
+
+        const closeTargets = this.getTargets('close')
+        closeTargets?.forEach((target) => {
+            target.addEventListener('click', this.hide)
+        })
+    }
+
+    disconnectedCallback() {
+        const targets = this.getTargets('open')
+        targets?.forEach((target) =>
+            target.removeEventListener('click', this.display),
+        )
+        const closeTargets = this.getTargets('close')
+        closeTargets?.forEach((target) =>
+            target.removeEventListener('click', this.hide),
+        )
+    }
+}
