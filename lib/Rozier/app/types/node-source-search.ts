72887b3d6e51950b42b952ef8253f27bdb86cb19
timothe joubert <timothejoubert26@gmail.com>
2025-12-19 18:01:13 +0100

    refactor: set all keys optionnal

--- lib/Rozier/app/types/node-source-search.ts
+++ lib/Rozier/app/types/node-source-search.ts
@@ -1,25 +1,25 @@
 interface Thumbnail {
-    alt: string | null
-    embedId: string | null
-    embedPlatform: string | null
-    hotspot: string | null
-    imageAverageColor: string
-    imageHeight: number
-    imageWidth: number
-    mediaDuration: number
-    mimeType: string
-    processable: boolean
-    relativePath: string
-    type: string
-    url: string
+    alt?: string | null
+    embedId?: string | null
+    embedPlatform?: string | null
+    hotspot?: string | null
+    imageAverageColor?: string
+    imageHeight?: number
+    imageWidth?: number
+    mediaDuration?: number
+    mimeType?: string
+    processable?: boolean
+    relativePath?: string
+    type?: string
+    url?: string
 }
 
 export interface NodeSourceSearch {
-    classname: string
-    color: string
-    displayable: string
-    editItem: string
-    id: number
-    published: boolean
-    thumbnail: Thumbnail
+    classname?: string
+    color?: string
+    displayable?: string
+    editItem?: string
+    id?: number
+    published?: boolean
+    thumbnail?: Thumbnail
 }
