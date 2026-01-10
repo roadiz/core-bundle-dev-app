80d96ca787750636ea78dee4160f1428371e1312
timothe joubert <timothejoubert26@gmail.com>
2026-01-10 17:54:49 +0100

    style: update story

--- lib/Rozier/stories/RzThemeSwitcher.stories.ts
+++ lib/Rozier/stories/RzThemeSwitcher.stories.ts
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from '@storybook/html-vite'
 import { rzButtonRenderer } from '~/utils/component-renderer/rzButton'
 import { rzPopoverRenderer } from '~/utils/storybook/renderer/rzPopover'
 import { rzElement } from '~/utils/component-renderer/rzElement'
+import { rzIconRenderer } from '~/utils/component-renderer/rzIcon'
 
 export type Args = {
     legend: string
@@ -20,12 +21,14 @@ type Story = StoryObj<Args>
 
 function radioRenderer(options: {
     label: string
+    icon: string
     value: string
     checked?: boolean
 }) {
-    const wrapper = document.createElement('div')
     const label = document.createElement('label')
+    label.classList.add('rz-theme-fieldset__item')
     label.textContent = options.label
+
     const input = document.createElement('input')
     input.type = 'radio'
     input.name = 'theme'
@@ -33,33 +36,45 @@ function radioRenderer(options: {
     if (options.checked) {
         input.checked = true
     }
+
+    const icon = rzIconRenderer({ class: options.icon })
+
     label.appendChild(input)
-    wrapper.appendChild(label)
-    return wrapper
+    label.appendChild(icon)
+    return label
 }
 
 function rzThemeSwitcherRenderer(args: Args) {
     const target = rzButtonRenderer({
-        label: 'Appeareance',
-        iconClass: 'rz-icon--theme-switcher',
+        is: 'rz-button',
+        iconClass: 'rz-icon-ri--color-filter-line',
+        attributes: {
+            'aria-label': 'Choose app color scheme',
+        },
     })
 
     const popoverElement = rzElement({
         tag: 'fieldset',
         is: 'rz-theme-fieldset',
-        innerHTML: `<legend>${args.legend}</legend>`,
+        innerHTML: `<legend class="rz-visually-hidden">${args.legend}</legend>`,
     })
 
     ;[
-        { label: 'System', value: 'light dark', checked: true },
-        { label: 'Light', value: 'light' },
-        { label: 'Dark', value: 'dark' },
+        {
+            label: 'System',
+            icon: 'rz-icon-ri--computer-line',
+            value: 'light dark',
+            checked: true,
+        },
+        { label: 'Light', icon: 'rz-icon-ri--sun-line', value: 'light' },
+        { label: 'Dark', icon: 'rz-icon-ri--moon-line', value: 'dark' },
     ].forEach((options) => {
         popoverElement.appendChild(radioRenderer(options))
     })
 
     const { popover } = rzPopoverRenderer({
         targetElement: { element: target },
+        offset: 12,
         placement: 'bottom-start',
         popoverElement: {
             id: 'theme-switcher-popover',
