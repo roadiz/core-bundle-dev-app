e042c083ccf7e99b8515a0bd09d967e29e024e86
timothe joubert <timothejoubert26@gmail.com>
2026-01-30 16:05:46 +0100

    style: adapt component and add integration

---
+++ lib/Rozier/app/custom-elements/RzColorSchemeSwitcher.ts
@@ -0,0 +1,57 @@
+export default class RzColorSchemeSwitcher extends HTMLElement {
+    constructor() {
+        super()
+
+        this.onCommand = this.onCommand.bind(this)
+    }
+
+    onCommand(event: CommandEvent) {
+        switch (event.command) {
+            case '--update-color-scheme':
+                this.onButtonClick(event)
+                break
+        }
+    }
+
+    onButtonClick(event: CommandEvent) {
+        const button = event.source as HTMLButtonElement
+        const value = button.getAttribute('data-value') || undefined
+
+        this.querySelectorAll(`button[command='${event.command}']`).forEach(
+            (btn) => {
+                if (btn === button) {
+                    btn.setAttribute('aria-current', 'true')
+                    btn.classList.add('rz-dropdown__item--selected')
+                } else {
+                    btn.removeAttribute('aria-current')
+                    btn.classList.remove('rz-dropdown__item--selected')
+                }
+            },
+        )
+
+        this.updateColorScheme(value)
+    }
+
+    updateColorScheme(value: string | undefined) {
+        if (!value) return
+        document.documentElement.setAttribute('data-theme', value)
+
+        let meta = document.querySelector<HTMLMetaElement>(
+            'meta[name="theme-color"]',
+        )
+        if (!meta) {
+            meta = document.createElement('meta')
+            meta.name = 'theme-color'
+            document.head.appendChild(meta)
+        }
+        meta.content = value
+    }
+
+    connectedCallback() {
+        this.addEventListener('command', this.onCommand)
+    }
+
+    disconnectedCallback() {
+        this.removeEventListener('command', this.onCommand)
+    }
+}
--- lib/Rozier/stories/RzThemeSwitcher.stories.ts
+++ lib/Rozier/stories/RzThemeSwitcher.stories.ts
@@ -1,121 +1,72 @@
 import type { Meta, StoryObj } from '@storybook/html-vite'
-import { rzButtonRenderer } from '~/utils/component-renderer/rzButton'
 import { rzPopoverRenderer } from '~/utils/storybook/renderer/rzPopover'
-import { rzElement } from '~/utils/component-renderer/rzElement'
-import { rzIconRenderer } from '~/utils/component-renderer/rzIcon'
 
 export type Args = {
     legend: string
 }
 
 const meta: Meta<Args> = {
-    title: 'Components/ThemeSwitcher',
+    title: 'Components/ColorSchemeSwitcher',
     tags: ['autodocs'],
-    args: {
-        legend: 'Choose the app color scheme',
-    },
 }
 
 export default meta
 type Story = StoryObj<Args>
 
-function radioRenderer(options: {
-    label: string
-    icon: string
-    value: string
-    checked?: boolean
-}) {
-    const label = document.createElement('label')
-    label.classList.add('rz-theme-fieldset__item')
-    label.textContent = options.label
-
-    const input = document.createElement('input')
-    input.type = 'radio'
-    input.name = 'theme'
-    input.value = options.value
-    if (options.checked) {
-        input.checked = true
-    }
-
-    const icon = rzIconRenderer({ class: options.icon })
-
-    label.appendChild(input)
-    label.appendChild(icon)
-    return label
-}
-
-const radioElements = [
-    {
-        label: 'System',
-        icon: 'rz-icon-ri--computer-line',
-        value: 'light dark',
-        checked: true,
-    },
-    { label: 'Light', icon: 'rz-icon-ri--sun-line', value: 'light' },
-    { label: 'Dark', icon: 'rz-icon-ri--moon-line', value: 'dark' },
-].map((options) => {
-    return radioRenderer(options)
-})
-
-function rzThemeSwitcherRenderer(args: Args) {
-    const target = rzButtonRenderer({
-        is: 'rz-button',
-        iconClass: 'rz-icon-ri--color-filter-line',
-        attributes: {
-            'aria-label': 'Ouvrir le sélecteur de thème',
-        },
-    })
-
-    const popoverElement = rzElement({
-        tag: 'fieldset',
-        is: 'rz-theme-fieldset',
-        innerHTML: `
-            <legend class="rz-visually-hidden">${args.legend}</legend>
-                ${radioElements
-                    .map((el) => {
-                        return el.outerHTML
-                    })
-                    .join('')}
-        `,
-    })
-
-    // const popoverElement = rzElement({
-    //     tag: 'fieldset',
-    //     is: 'rz-theme-fieldset',
-    //     attributes: {
-    //         class: 'rz-dropdown',
-    //     },
-    //     innerHTML: `
-    //         <div class="rz-dropdown__head">
-    //             <div class="rz-dropdown__title">Select a theme</div>
-    //         </div>
-    //         <legend class="rz-visually-hidden">${args.legend}</legend>
-    //         <div class="rz-dropdown__list">
-    //             ${radioElements
-    //                 .map((el) => {
-    //                     el.classList.add('rz-dropdown__item')
-    //                     return el.outerHTML
-    //                 })
-    //                 .join('')}
-    //         </div>
-    //     `,
-    // })
-
-    const { popover } = rzPopoverRenderer({
-        targetElement: { element: target },
-        offset: 12,
-        placement: 'bottom-start',
-        popoverElement: {
-            id: 'theme-switcher-popover',
-            element: popoverElement,
-        },
-    })
-
-    return popover
-}
-
 export const Default: Story = {
-    render: (args) => {
-        return rzThemeSwitcherRenderer(args)
+    render: () => {
+        const id = 'color-scheme-switcher-storybook'
+        const content = document.createElement('rz-color-scheme-switcher')
+        content.innerHTML = `
+<menu class="rz-dropdown__list">
+    <li class="">
+                    <button class="rz-dropdown__item" type="button" data-value="system" command="--update-color-scheme"
+                    commandfor="${id}"
+                    >
+                        <div class="rz-dropdown__item__text-wrapper">
+                                    <span class="rz-dropdown__item__label">color.system</span>
+                                            </div>
+                                        <span class="rz-dropdown__item__icon rz-icon-ri--computer-line"></span>
+                                </button>
+    </li>
+
+
+
+    <li class="">
+                    <button class="rz-dropdown__item rz-dropdown__item--selected" type="button" data-value="light" command="--update-color-scheme" commandfor="${id}">
+                        <div class="rz-dropdown__item__text-wrapper">
+                                    <span class="rz-dropdown__item__label">color.light</span>
+                                            </div>
+                                        <span class="rz-dropdown__item__icon rz-icon-ri--sun-line"></span>
+                                </button>
+    </li>
+
+
+
+    <li class="">
+                    <button class="rz-dropdown__item" type="button" data-value="dark" command="--update-color-scheme" commandfor="${id}">
+                        <div class="rz-dropdown__item__text-wrapper">
+                                    <span class="rz-dropdown__item__label">color.dark</span>
+                                            </div>
+                                        <span class="rz-dropdown__item__icon rz-icon-ri--moon-line"></span>
+                                </button>
+    </li>
+
+			        </menu>
+        `
+
+        const button = document.createElement('button')
+        button.textContent = 'Switch Theme'
+
+        const { popover } = rzPopoverRenderer({
+            popoverElement: {
+                tag: 'rz-color-scheme-switcher',
+                id: id,
+                element: content,
+            },
+            targetElement: { element: button },
+        })
+
+        return popover
     },
 }
