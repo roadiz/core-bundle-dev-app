da61abd7f17d275dac7fde3b5e44800494d7c38e
timothe joubert <timothejoubert26@gmail.com>
2026-02-13 15:53:24 +0100

    style: add persistant user color scheme

--- lib/Rozier/app/custom-elements/RzColorSchemeSwitcher.ts
+++ lib/Rozier/app/custom-elements/RzColorSchemeSwitcher.ts
@@ -1,4 +1,6 @@
 export default class RzColorSchemeSwitcher extends HTMLElement {
+    private readonly storageKey = 'rz-color-scheme'
+
     constructor() {
         super()
 
@@ -17,28 +19,42 @@ export default class RzColorSchemeSwitcher extends HTMLElement {
         const button = event.source as HTMLButtonElement
         const value = button.getAttribute('data-value') || undefined
 
-        this.querySelectorAll(`button[command='${event.command}']`).forEach(
-            (btn) => {
-                if (btn === button) {
-                    btn.setAttribute('aria-current', 'true')
-                    btn.classList.add('rz-dropdown__item--selected')
-                } else {
-                    btn.removeAttribute('aria-current')
-                    btn.classList.remove('rz-dropdown__item--selected')
-                }
-            },
-        )
+        this.updateSelectedButton(event.command, value)
 
         this.updateColorScheme(value)
     }
 
     updateColorScheme(value: string | undefined) {
-        if (!value) return
+        if (!value || value === 'system') {
+            document.documentElement.removeAttribute('data-theme')
+            localStorage.removeItem(this.storageKey)
+            return
+        }
+
         document.documentElement.setAttribute('data-theme', value)
+        localStorage.setItem(this.storageKey, value)
+    }
+
+    updateSelectedButton(command: string, value: string | null | undefined) {
+        this.querySelectorAll(`button[command='${command}']`).forEach((btn) => {
+            if (btn.getAttribute('data-value') === value) {
+                btn.setAttribute('aria-current', 'true')
+                btn.classList.add('rz-dropdown__item--selected')
+            } else {
+                btn.removeAttribute('aria-current')
+                btn.classList.remove('rz-dropdown__item--selected')
+            }
+        })
     }
 
     connectedCallback() {
         this.addEventListener('command', this.onCommand)
+
+        const storedValue = localStorage.getItem(this.storageKey)
+        if (storedValue === 'light' || storedValue === 'dark') {
+            this.updateSelectedButton('--update-color-scheme', storedValue)
+            this.updateColorScheme(storedValue)
+        }
     }
 
     disconnectedCallback() {
